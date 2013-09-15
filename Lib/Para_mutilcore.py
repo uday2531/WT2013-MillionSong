6771e43942baf4d9e18ca10ad1a081024fb50074
Eminemya <weiddoonngglai@gmail.com>
2013-09-15 18:55:31 -0400

    WriteSong

--- Lib/Para_mutilcore.py
+++ Lib/Para_mutilcore.py
@@ -11,15 +11,20 @@ c.close()
 """
 # Write_Song([0,1,10,'../data/kaggle_visible_evaluation_triplets.txt','m1_order.txt'])
 def Get_UserLine(L):
-    result = os.popen("awk 'NR >= "+str(L[1])+" && NR <= "+str(L[2])+"' "+L[3])
-    cc = L[1]
-    user = result[0][0]
-    users = [user,cc]
-    for line in result[1:]:
-        cc++
-        if line[0]!=user:
-            users.append(cc)
-            user = line[0]
+    fp = open(L[2])
+    user = ''
+    users = []
+    for i, lines in enumerate(fp):
+        if (i > L[0] and i <= L[1]) or i==0:
+            line = lines.split()
+            if line[0]!=user:
+                if user=='':
+                    users.append(line[0])
+                users.append(i)
+                user = line[0]
+        elif i>L[1]:
+            break
+    fp.close()
     users.append(user)
     return users
 
@@ -43,7 +48,7 @@ def Write_Song(L):
 
 
 num_proc = 1;
-Write_Song([0,1,10,'../data/kaggle_visible_evaluation_triplets.txt','m1_order.txt'])
+#Write_Song([0,1,10,'../data/kaggle_visible_evaluation_triplets.txt','m1_order.txt'])
 # Build a pool of 8 processes
 pool = Pool(processes=num_proc,)
 
@@ -55,9 +60,13 @@ len_test = int(tmp_re.readline().split()[0])
 avg_test = len_test/num_proc+1
 text_p = [[i*avg_test,(i+1)*avg_test,test_file] for i in range(num_proc)]
 text_p[-1][1]=len_test
+text_p[0][1] = 100
 
 tmp_re = pool.map(Get_UserLine, text_p)
-userlines= tmp_re[1:-1]
+#tmp_re = Get_UserLine(text_p[0])
+print tmp_re
+userlines= tmp_re[0][1:-1]
+"""
 for i in range(1,len(tmp_re)):
     if tmp_re[i][0]==tmp_re[i-1][-1]:        
         userlines.append(tmp_re[i][2:-1])
